e48586104f3d2b55bf43c9f1afbeb7129f473f53
Unknown <robin.dard@inserm.fr>
2019-06-03 12:20:56 +0200

    code_spotDis et DLC

--- src/python_TIPE_JD.py
+++ src/python_TIPE_JD.py
@@ -1,5 +1,10 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import scipy.stats as scistats
+import seaborn as sns
+import hdbscan
+from sklearn.manifold import TSNE as t_sne
+import pandas as pd
 
 class Epoch:
     """
@@ -43,6 +48,8 @@ class Epoch:
             for neuron_2 in np.arange(neuron_1+1, self.n_neurons):
                 self.compute_normalized_cross_correlation(neuron_1, neuron_2)
 
+
+
     def compute_normalized_cross_correlation(self, neuron_1, neuron_2):
         """
         compute for a pair of neurons the normalize cross_correlation with respect of lag
@@ -73,6 +80,8 @@ class Epoch:
             cross_correlation_array[lag_time_index] = corr_value
 
         show_corr_bar_chart = (np.sum(cross_correlation_array) > 0)
+        # if len(np.where(cross_correlation_array < 0)[0]) > 0:
+        #     print(f"len neg {len(np.where(cross_correlation_array < 0)[0])}")
         # comment next line to show the correlation plot when the sum is > 0
         show_corr_bar_chart = False
         if show_corr_bar_chart:
@@ -100,9 +109,10 @@ class Epoch:
         # in one array we save the indices of the lags with positive corr (we could also keep a binary
         # array with 1 in time lags where a corr value is > 0 (might use less memory)
         # in another one we save the corr values as float value
-        lags_indices = np.where(normalized_cross_correlation_array > 0)[0]
-        cross_correlation_values = normalized_cross_correlation_array[normalized_cross_correlation_array > 0]
-        self.neurons_pairs_cross_correlation[(neuron_1, neuron_2)] = [lags_indices, cross_correlation_values]
+        # lags_indices = np.where(normalized_cross_correlation_array > 0)[0]
+        #cross_correlation_values = normalized_cross_correlation_array[normalized_cross_correlation_array > 0]
+        # self.neurons_pairs_cross_correlation[(neuron_1, neuron_2)] = [lags_indices, cross_correlation_values]
+        self.neurons_pairs_cross_correlation[(neuron_1, neuron_2)] = normalized_cross_correlation_array
 
     def __hash__(self):
         """
@@ -125,23 +135,27 @@ def load_data():
     :return: return a 2D binary array representing a raster. Axis 0 (lines) represents the neurons (cells) and axis 1
     (columns) represent the frames (in our case sampling is approximatively 10Hz, so 100 ms by frame).
     """
-    path_data = "/Users/pappyhammer/Documents/academique/these_inmed/robin_michel_data/data/data_TIPE/"
-    loaded_data = np.load(path_data + "P60_a529_2015_02_25_rasters_reduced.npz")
-    # put to true to print the name of the variables available in this npz file
-    print_variable_names = False
-    if print_variable_names:
-        for key in loaded_data.keys():
-            print(f"Variable in npz file: {key}")
+    # path_data = "/Users/pappyhammer/Documents/academique/these_inmed/robin_michel_data/data/data_TIPE/"
+    # loaded_data = np.load(path_data + "P60_a529_2015_02_25_rasters_reduced.npz")
+    # # put to true to print the name of the variables available in this npz file
+    # print_variable_names = False
+    # if print_variable_names:
+    #     for key in loaded_data.keys():
+    #         print(f"Variable in npz file: {key}")
     # spike_nums is a binary 2D array containing the onsets (1 if onset)
-    spike_nums = loaded_data["spike_nums"]
+    #spike_nums = loaded_data["spike_nums"]
     # spike_nums is a binary 2D array containing the active period of neuron (a neuron is considered active from its
     # onset to its peak during a transient, thus when the value is 1 the neuron is active)
-    spike_nums_dur = loaded_data["spike_nums_dur"]
+    spike_nums_dur = np.load('D:/Robin/data_hne/data/p41/p41_19_04_30_a000/predictions/P41_19_04_30_a000_filtered_predicted_raster_dur_meso_v1_epoch_9.npy')
+    #spike_nums_dur = loaded_data["spike_nums_dur"]
+    # n_cells, n_frames = spike_nums_dur.shape
+    spike_nums_dur = spike_nums_dur[:200, :7500]
 
     return spike_nums_dur
 
 
-def compute_emd_between_2_pair_of_neurons(epoch_1, epoch_2, neurons_pair):
+
+def compute_emd_for_a_pair_of_neurons_between_2_epochs(epoch_1, epoch_2, neurons_pair):
     """
 
     :param epoch_1: first epoch (Epoch instance)
@@ -150,8 +164,10 @@ def compute_emd_between_2_pair_of_neurons(epoch_1, epoch_2, neurons_pair):
     :return:
     """
     # TODO: write the code.
-
-    return 0.
+    return scistats.wasserstein_distance(epoch_1.neurons_pairs_cross_correlation[neurons_pair],
+                                         np.arange(len(epoch_2.neurons_pairs_cross_correlation[neurons_pair])),
+                                         u_weights=epoch_1.neurons_pairs_cross_correlation[neurons_pair],
+                                         v_weights=epoch_2.neurons_pairs_cross_correlation[neurons_pair])
 
 
 def compute_spotdis_between_2_epochs(epoch_1, epoch_2):
@@ -168,13 +184,19 @@ def compute_spotdis_between_2_epochs(epoch_1, epoch_2):
     total_count = 0
     emd_sum = 0
     for neurons_pair in neurons_pairs:
-        emd_value = compute_emd_between_2_pair_of_neurons(epoch_1=epoch_1, epoch_2=epoch_2, neurons_pair=neurons_pair)
         # we get the lags and correlations values of both epoch to average the emd
-        lags_corr_values_e_1 = epoch_1.neurons_pairs_cross_correlation[neurons_pair]
-        lags_corr_values_e_2 = epoch_2.neurons_pairs_cross_correlation[neurons_pair]
-        # if one of the epoch pair of neurons has an empty correlation hisogram, then we don't count it
-        if (len(lags_corr_values_e_1[0]) == 0) or (len(lags_corr_values_e_2[0]) == 0):
+        # lags_corr_values_e_1, c_1 = epoch_1.neurons_pairs_cross_correlation[neurons_pair]
+        # lags_corr_values_e_2, c_2 = epoch_2.neurons_pairs_cross_correlation[neurons_pair]
+        # # if one of the epoch pair of neurons has an empty correlation hisogram, then we don't count it
+        # if (len(lags_corr_values_e_1[0]) == 0) or (len(lags_corr_values_e_2[0]) == 0):
+        #     continue
+
+        if (np.sum(epoch_1.neurons_pairs_cross_correlation[neurons_pair]) == 0) or \
+                (np.sum(epoch_2.neurons_pairs_cross_correlation[neurons_pair]) == 0):
             continue
+
+        emd_value = compute_emd_for_a_pair_of_neurons_between_2_epochs(epoch_1=epoch_1, epoch_2=epoch_2,
+                                                                       neurons_pair=neurons_pair)
         total_count += 1
         emd_sum += emd_value
     # computing the average
@@ -186,8 +208,8 @@ def compute_spotdis_between_2_epochs(epoch_1, epoch_2):
         # TODO: in that case total_count will be equal to zeros, for now we put spotdis value to 1 (should it be zero ?)
         # In the paper, they assume, that for each pair of epochs k and m,
         # there is at least one pair of neurons in which both neurons fired in both epochs k and m
-        print("total_count is equal to zero")
         spotdis_value = 1
+
 
     return spotdis_value
 
@@ -204,7 +226,7 @@ def main():
     # number of neurons
     n_neurons = spike_nums.shape[0]
     # we fix the length of an epoch, knowing than 1 frame is equal to 100 ms approximately
-    len_epoch = 50
+    len_epoch = 100
     # then computing the number of epoch in our raster
     n_epochs = n_frames // len_epoch
     # to make things easy for now, the number of frames should be divisible by the length of epochs
@@ -224,12 +246,15 @@ def main():
 
     # then we compute for each epoch the n_neurons(n_neurons − 1)/2 normalized cross-correlations between each pair of
     # neurons
+    print(f"Computing all normalized cross correlation")
     for epoch in epochs:
         epoch.compute_all_normalized_cross_correlation()
+    print(f"Computation of all normalized cross correlation done")
 
     # a 2d array that contains the spotdis value for each pair of epochs
     spotdis_values = np.zeros((n_epochs, n_epochs))
 
+    print(f"Computing of all spotdis value")
     for epoch_1_index in np.arange(n_epochs - 1):
         for epoch_2_index in np.arange(epoch_1_index+1, n_epochs):
             epoch_1 = epochs[epoch_1_index]
@@ -237,6 +262,139 @@ def main():
             spotdis_value = compute_spotdis_between_2_epochs(epoch_1=epoch_1, epoch_2=epoch_2)
             spotdis_values[epoch_1_index, epoch_2_index] = spotdis_value
             spotdis_values[epoch_2_index, epoch_1_index] = spotdis_value
+    print(f"total_count is equal to zero for {len(spotdis_values==0)} spotdis values")
+    print(f"Computation of all spotdis values is done")
+
+
+    ## DO HDBSCAN CLUSTERING ON DISSIMILARITY MATRIX (SPOTDIS VALUES) ##
+
+    clusterer = hdbscan.HDBSCAN(algorithm='best', alpha=1.0, approx_min_span_tree=True,
+                                gen_min_span_tree=False, leaf_size=40,
+                                metric='precomputed', min_cluster_size=2, min_samples=None, p=None)
+    # metric='precomputed' euclidean
+    clusterer.fit(spotdis_values)
+
+    labels = clusterer.labels_
+    #print(f"labels.shape: {labels.shape}")
+    print(f"N clusters hdbscan: {labels.max()+1}")
+    #print(f"labels: {labels}")
+    print(f"With no clusters hdbscan: {len(np.where(labels == -1)[0])}")
+
+    if labels.max()+1 > 0:
+        n_clusters = labels.max() + 1
+
+    if n_clusters > 0:
+        n_epoch_by_cluster = [len(np.where(labels == x)[0]) for x in np.arange(n_clusters)]
+
+    print(f"Number of epochs by clusters hdbscan: {' '.join(map(str, n_epoch_by_cluster))}")
+
+    spotdis_order = np.copy(spotdis_values)
+    spotdis_order = spotdis_order[np.argsort(labels), :]
+    spotdis_order = spotdis_order[:, np.argsort(labels)]
+
+    # Generate figure: dissimilarity matrice ordered by cluster
+    svm = sns.heatmap(spotdis_order)
+    fig = svm.get_figure()
+    # plt.show()
+    save_formats = ["pdf"]
+    if isinstance(save_formats, str):
+        save_formats = [save_formats]
+
+    path_results = "D:/Robin/data_hne/data/p41/p41_19_04_30_a000"
+    for save_format in save_formats:
+        fig.savefig(f'{path_results}/heatmap_hdbscan_clustering_order'
+                    f'.{save_format}',
+                    format=f"{save_format}",
+                    facecolor=fig.get_facecolor())
+    plt.close()
+
+
+    ## DO T-SNE CLUSTERING ON SPOTDIS VALUES ##
+
+    tsne = t_sne(n_components=2, verbose=1, perplexity=40, n_iter=300)
+    tsne_results = tsne.fit_transform(spotdis_values)
+
+    # first figure: plot t-sne without color
+    df_subset = pd.DataFrame()
+    df_subset['tsne-2d-one'] = tsne_results[:, 0]
+    df_subset['tsne-2d-two'] = tsne_results[:, 1]
+    df_subset['color'] = labels
+    plt.figure(figsize=(16, 10))
+    svm = sns.scatterplot(
+        x="tsne-2d-one", y="tsne-2d-two",
+        data=df_subset,
+        legend="full",
+        alpha=1
+    )
+    fig = svm.get_figure()
+
+    path_results = "D:/Robin/data_hne/data/p41/p41_19_04_30_a000"
+    for save_format in save_formats:
+        fig.savefig(f'{path_results}/tsne_cluster'
+                    f'.{save_format}',
+                    format=f"{save_format}",
+                    facecolor=fig.get_facecolor())
+    plt.close()
+
+    # second figure: plot t-sne with color from previous hdbscan result
+    df_subset = pd.DataFrame()
+    df_subset['tsne-2d-one'] = tsne_results[:, 0]
+    df_subset['tsne-2d-two'] = tsne_results[:, 1]
+    df_subset['color'] = labels
+
+    plt.figure(figsize=(16, 10))
+    svm = sns.scatterplot(
+        x="tsne-2d-one", y="tsne-2d-two",
+        hue="color",
+        palette=sns.color_palette("hls", labels.max() + 2),
+        data=df_subset,
+        legend="full",
+        alpha=1
+    )
+    fig = svm.get_figure()
+
+    path_results = "D:/Robin/data_hne/data/p41/p41_19_04_30_a000"
+    for save_format in save_formats:
+        fig.savefig(f'{path_results}/tsne_colors_from_previous_hdbscan_clustering'
+                    f'.{save_format}',
+                    format=f"{save_format}",
+                    facecolor=fig.get_facecolor())
+    plt.close()
+
+    # DO CLUSTERING ON T-SNE RESULTS TO COLOR THE T-SNE FIGURE ##
+
+    clusterer = hdbscan.HDBSCAN(algorithm='best', alpha=1.0, approx_min_span_tree=True,
+                                gen_min_span_tree=False, leaf_size=40,
+                                metric='euclidean', min_cluster_size=3, min_samples=None, p=None)
+    clusterer.fit(tsne_results)
+    labels_hdbscan_on_tsne = clusterer.labels_
+    print(f"N clusters hdbscan on t-sne results: {labels_hdbscan_on_tsne.max()+1}")
+    #print(f"labels: {labels_hdbscan_on_tsne}")
+
+    df_subset = pd.DataFrame()
+    df_subset['tsne-2d-one'] = tsne_results[:, 0]
+    df_subset['tsne-2d-two'] = tsne_results[:, 1]
+    df_subset['color'] = labels_hdbscan_on_tsne
+
+    plt.figure(figsize=(16, 10))
+    svm = sns.scatterplot(
+        x="tsne-2d-one", y="tsne-2d-two",
+        hue="color",
+        palette=sns.color_palette("hls", labels_hdbscan_on_tsne.max()+2),
+        data=df_subset,
+        legend="full",
+        alpha=1
+    )
+    # plt.show()
+    fig = svm.get_figure()
+
+    path_results = "D:/Robin/data_hne/data/p41/p41_19_04_30_a000"
+    for save_format in save_formats:
+        fig.savefig(f'{path_results}/tsne_colors_from_post_tsne_clustering'
+                    f'.{save_format}',
+                    format=f"{save_format}",
+                    facecolor=fig.get_facecolor())
+    plt.close()
 
 
 # periode1 = date1
---
+++ src/test_dlc.py
@@ -0,0 +1,42 @@
+import deeplabcut
+
+def main():
+
+    #deeplabcut.create_new_project("test_2", "Robin", ["D:/Robin/test _movie_DLC/test_training/test_2.avi"], copy_videos=True)
+
+    # deeplabcut.extract_frames("D:/Robin/test _movie_DLC/test_training/test_2-Robin-2019-06-03/config.yaml",
+    #                           mode='automatic', algo='kmeans', crop=False, userfeedback=True, cluster_step=1,
+    #                           cluster_resizewidth=30, cluster_color=False, opencv=True)
+
+    # deeplabcut.label_frames("D:/Robin/test _movie_DLC/test_training/test_2-Robin-2019-06-03/config.yaml")
+
+    # deeplabcut.check_labels("D:/Robin/test _movie_DLC/test_training/test_2-Robin-2019-06-03/config.yaml")
+
+    # deeplabcut.create_training_dataset("D:/Robin/test _movie_DLC/test_training/test_2-Robin-2019-06-03/config.yaml",
+    #                                    num_shuffles=1, Shuffles=None, windows2linux=False, trainIndices=None,
+    #                                    testIndices=None)
+
+    # deeplabcut.train_network("D:/Robin/test _movie_DLC/test_training/test_2-Robin-2019-06-03/config.yaml", shuffle=1,
+    #                          trainingsetindex=0, gputouse=0, max_snapshots_to_keep=5, autotune=False,
+    #                          displayiters=1000, saveiters=None, maxiters=None)
+
+    # deeplabcut.evaluate_network("D:/Robin/test _movie_DLC/p11_test/test-Robin-2019-05-31/config.yaml", Shuffles=[1],
+    #                             plotting=None, show_errors=True, comparisonbodyparts="all", gputouse=0)
+
+    # deeplabcut.analyze_videos("D:/Robin/test _movie_DLC/p11_test/test-Robin-2019-05-31/config.yaml",
+    #                           ["D:/Robin/test _movie_DLC/p11_test/p11_test.avi"], videotype='avi', shuffle=1,
+    #                           trainingsetindex=0, gputouse=0, save_as_csv=False, destfolder=None, cropping=None)
+
+    # deeplabcut.create_labeled_video("D:/Robin/test _movie_DLC/p11_test/test-Robin-2019-05-31/config.yaml",
+    #                                 ["D:/Robin/test _movie_DLC/p11_test/p11_test.avi"],
+    #                                 videotype='avi', shuffle=1, trainingsetindex=0, filtered=False, save_frames=False,
+    #                                 Frames2plot=None, delete=False, displayedbodyparts='all', codec='mp4v',
+    #                                 outputframerate=None, destfolder=None)
+
+    deeplabcut.plot_trajectories("D:/Robin/test _movie_DLC/p11_test/test-Robin-2019-05-31/config.yaml",
+                                 ["D:/Robin/test _movie_DLC/p11_test/p11_test.avi"], videotype='.avi', shuffle=1,
+                                 trainingsetindex=0, filtered=False, showfigures=False, destfolder=None)
+
+
+
+main()
