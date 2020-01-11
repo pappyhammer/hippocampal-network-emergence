ca70b12402edb48060faab775f68abcbca6135d0
Julien Denis <julien.denis3@gmail.com>
2020-01-11 22:45:48 +0100

    Adding cell type classifier to deepcinac

--- src/deep_cinac_training.py
+++ src/deep_cinac_training.py
@@ -31,29 +31,60 @@ if __name__ == '__main__':
     results_path = os.path.join(results_path, time_str)
     os.mkdir(results_path)
 
-    """
-    To start training from a full saved model from another training, it is necessary to:
-    - During the previous training, to put as argument: save_only_the_weitghs=False
-    - then specify the .h5 containing the model using partly_trained_model
-    - finally setting the learning rate so it is the same as the last epoch trained, using learning_rate_start
-    """
+    cell_type_classifier_mode = True
 
-    partly_trained_model = "/media/julien/Not_today/hne_not_today/data/test_cinac_gui/transient_classifier_full_model_02-0.9817.h5"
-    cinac_model = CinacModel(results_path=results_path, n_epochs=3, verbose=1, batch_size=4,
-                               # partly_trained_model=partly_trained_model,
-                               #  learning_rate_start = 0.001,
-                             save_only_the_weitghs=False
-                             )
+    if not cell_type_classifier_mode:
+        """
+        To start training from a full saved model from another training, it is necessary to:
+        - During the previous training, to put as argument: save_only_the_weitghs=False
+        - then specify the .h5 containing the model using partly_trained_model
+        - finally setting the learning rate so it is the same as the last epoch trained, using learning_rate_start
+        """
 
-    # artificial_movie_1 = os.path.join(data_path, "test_cinac_gui/artificial_movie_1_cell_0.cinac")
+        partly_trained_model = "/media/julien/Not_today/hne_not_today/data/test_cinac_gui/transient_classifier_full_model_02-0.9817.h5"
+        cinac_model = CinacModel(results_path=results_path, n_epochs=3, verbose=1, batch_size=4,
+                                 cell_type_classifier_mode=cell_type_classifier_mode,
+                                   # partly_trained_model=partly_trained_model,
+                                   #  learning_rate_start = 0.001,
+                                 save_only_the_weitghs=False
+                                 )
 
-    cinac_dir_name = os.path.join(data_path, "cinac_ground_truth/for_training")
+        # artificial_movie_1 = os.path.join(data_path, "test_cinac_gui/artificial_movie_1_cell_0.cinac")
 
-    cinac_model.add_input_data_from_dir(dir_name=cinac_dir_name, verbose=1)
-    # cinac_model.add_input_data(cinac_file_names=artificial_movie_1)
+        cinac_dir_name = os.path.join(data_path, "cinac_ground_truth/for_training")
 
-    cinac_model.prepare_model(verbose=1)
-    # cinac_model.fit()
+        cinac_model.add_input_data_from_dir(dir_name=cinac_dir_name, verbose=1)
+        # cinac_model.add_input_data(cinac_file_names=artificial_movie_1)
+
+        cinac_model.prepare_model(verbose=1)
+        # cinac_model.fit()
+    else:
+        """
+                To start training from a full saved model from another training, it is necessary to:
+                - During the previous training, to put as argument: save_only_the_weitghs=False
+                - then specify the .h5 containing the model using partly_trained_model
+                - finally setting the learning rate so it is the same as the last epoch trained, using learning_rate_start
+                """
+
+        partly_trained_model = "/media/julien/Not_today/hne_not_today/data/test_cinac_gui/"
+        cinac_model = CinacModel(results_path=results_path, n_epochs=10, verbose=1, batch_size=8,
+                                 cell_type_classifier_mode=cell_type_classifier_mode,
+                                 window_len=200, max_n_transformations=2,
+                                 lstm_layers_size=[128], bin_lstm_size=128,
+                                 # partly_trained_model=partly_trained_model,
+                                 #  learning_rate_start = 0.001,
+                                 save_only_the_weitghs=False
+                                 )
+
+        # artificial_movie_1 = os.path.join(data_path, "test_cinac_gui/artificial_movie_1_cell_0.cinac")
+
+        cinac_dir_name = os.path.join(data_path, "cinac_cell_type_ground_truth/for_training")
+
+        cinac_model.add_input_data_from_dir(dir_name=cinac_dir_name, verbose=1)
+        # cinac_model.add_input_data(cinac_file_names=artificial_movie_1)
+
+        cinac_model.prepare_model(verbose=1)
+        cinac_model.fit()
 
 
 def just_for_test_to_forget():
