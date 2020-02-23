451ff0d6bba9bfdeab45fcaa4975cd2759318484
Julien <julien.denis3@gmail.com>
2020-02-23 01:14:13 +0100

    Update on dlc analyses

--- src/dlc_behavior_analysis.py
+++ src/dlc_behavior_analysis.py
@@ -11,7 +11,67 @@ from sklearn.manifold import TSNE
 import seaborn as sns
 
 
-def apply_tsne(data):
+def encode_frames_from_cicada(cicada_file, n_frames, side_to_exclude=None, no_behavior_str = "still"):
+    """
+    Return a list of len n_frames with str representing each frame behavior
+    Args:
+        cicada_file:
+        n_frames:
+
+    Returns:
+
+    """
+    npz_content = np.load(cicada_file)
+
+    tags = []
+    tags_index = dict()
+    n_tags = 0
+    for tag_name, value in npz_content.items():
+        if side_to_exclude in tag_name:
+            continue
+        if value.shape[1] > 0:
+            tags.append(tag_name)
+            tags_index[tag_name] = 0
+            n_tags += 1
+
+    interval_id = 0
+    intervals_array = np.zeros((len(tags), n_frames), dtype="int16")
+    interval_info_dict = dict()
+
+    for tag_name, value in npz_content.items():
+        if side_to_exclude in tag_name:
+            continue
+
+        for i in range(value.shape[1]):
+            intervals_array[tags_index[tag_name], value[0, i]:value[1, i]+1] = interval_id
+            interval_info_dict[interval_id] = value[1, i] - value[0, i] + 1
+            interval_id += 1
+
+    tags_by_frame = []
+    for frame_index in range(n_frames):
+        tags_indices = np.where(intervals_array[:, frame_index] > 0)[0]
+        if len(tags_indices) == 0:
+            tags_by_frame.append(no_behavior_str)
+        if len(tags_indices) == 1:
+            tags_by_frame.append(tags[tags_indices[0]])
+        else:
+            # then we select the shorter one
+            for i, tag_index in enumerate(tags_indices):
+                if i == 0:
+                    shorter_tag = tags[tag_index]
+                    interval_id = intervals_array[tag_index, frame_index]
+                    shorter_duration = interval_info_dict[interval_id]
+                else:
+                    interval_id = intervals_array[tag_index, frame_index]
+                    duration = interval_info_dict[interval_id]
+                    if duration < shorter_duration:
+                        shorter_duration = duration
+                        shorter_tag = tags[tag_index]
+            tags_by_frame.append(shorter_tag)
+    return tags_by_frame
+
+
+def apply_tsne(data, behavior_by_frame=None):
     sns.set(rc={'figure.figsize': (11.7, 8.27)})
     palette = sns.color_palette("hls", 10)
 
@@ -19,11 +79,13 @@ def apply_tsne(data):
     data_embedded = tsne.fit_transform(data)
     print(f"data_embedded.shape {data_embedded.shape}")
 
-    sns.scatterplot(data_embedded[:, 0], data_embedded[:, 1], legend='full', palette=palette)
+    sns.scatterplot(data_embedded[:, 0], data_embedded[:, 1], hue=behavior_by_frame,
+                    legend='full', palette=palette)
 
     plt.show()
 
-if __name__ == "__main__":
+
+def dlc_behavior_analysis():
     root_path = "/media/julien/Not_today/hne_not_today/data/behavior_movies/dlc_predictions/p5_19_12_10_0"
 
     data_path = os.path.join(root_path, "data")
@@ -69,12 +131,12 @@ if __name__ == "__main__":
         n_frames = len(pos_h5_data)
 
         # x, y, likelihood
-        pos_data_array = np.zeros((n_frames, len(bodyparts)*3))
+        pos_data_array = np.zeros((n_frames, len(bodyparts) * 3))
         for frame in np.arange(n_frames):
             pos_data_array[frame] = np.array(pos_h5_data[frame][1])
 
         # length, orientation, likelihood
-        skeleton_h5_data_array = np.zeros((n_frames, n_skeleton_parts*3))
+        skeleton_h5_data_array = np.zeros((n_frames, n_skeleton_parts * 3))
         for frame in np.arange(n_frames):
             skeleton_h5_data_array[frame] = np.array(skeleton_h5_data[frame][1])
 
@@ -98,8 +160,10 @@ if __name__ == "__main__":
 
             for frame in np.arange(n_frames):
                 skeleton_parts_pos_array[skeleton_index, 0, frame] = skeleton_h5_data_array[frame, skeleton_index * 3]
-                skeleton_parts_pos_array[skeleton_index, 1, frame] = skeleton_h5_data_array[frame, (skeleton_index * 3) + 1]
-                skeleton_parts_pos_array[skeleton_index, 2, frame] = skeleton_h5_data_array[frame, (skeleton_index * 3) + 2]
+                skeleton_parts_pos_array[skeleton_index, 1, frame] = skeleton_h5_data_array[
+                    frame, (skeleton_index * 3) + 1]
+                skeleton_parts_pos_array[skeleton_index, 2, frame] = skeleton_h5_data_array[
+                    frame, (skeleton_index * 3) + 2]
 
         # pcutoff
         # if 1 no thresholding
@@ -131,8 +195,8 @@ if __name__ == "__main__":
                         for frame_index in indices:
                             if frame_index == 0:
                                 # looking for next frame_index know
-                                if frame_index+1 in indices:
-                                    next_index = frame_index+1
+                                if frame_index + 1 in indices:
+                                    next_index = frame_index + 1
                                     while next_index in indices:
                                         next_index += 1
                                     parts_pos_array[part_index, 0, frame_index] = parts_pos_array[
@@ -140,23 +204,24 @@ if __name__ == "__main__":
                                     parts_pos_array[part_index, 1, frame_index] = parts_pos_array[
                                         part_index, 1, next_index]
                                 else:
-                                    parts_pos_array[part_index, 0, frame_index] = parts_pos_array[part_index, 0, frame_index+1]
-                                    parts_pos_array[part_index, 1, frame_index] = parts_pos_array[part_index, 1, frame_index+1]
+                                    parts_pos_array[part_index, 0, frame_index] = parts_pos_array[
+                                        part_index, 0, frame_index + 1]
+                                    parts_pos_array[part_index, 1, frame_index] = parts_pos_array[
+                                        part_index, 1, frame_index + 1]
                             else:
                                 x_values = parts_pos_array[part_index, 0]
                                 y_values = parts_pos_array[part_index, 1]
-                                if (frame_index +1) in indices or frame_index == (n_frames-1):
+                                if (frame_index + 1) in indices or frame_index == (n_frames - 1):
                                     # we take previous value
-                                    parts_pos_array[part_index, 0, frame_index] = x_values[frame_index-1]
-                                    parts_pos_array[part_index, 1, frame_index] = y_values[frame_index-1]
+                                    parts_pos_array[part_index, 0, frame_index] = x_values[frame_index - 1]
+                                    parts_pos_array[part_index, 1, frame_index] = y_values[frame_index - 1]
                                 else:
                                     # we take the mean
                                     parts_pos_array[part_index, 0, frame_index] = (x_values[frame_index - 1] +
-                                                                                  x_values[frame_index + 1]) / 2
+                                                                                   x_values[frame_index + 1]) / 2
                                     parts_pos_array[part_index, 1, frame_index] = (y_values[frame_index - 1] +
-                                                                                  y_values[frame_index + 1]) / 2
+                                                                                   y_values[frame_index + 1]) / 2
             print("")
-
 
         # print(f"pos data _i_table keys() {list(pos_h5_file['df_with_missing']['_i_table']['index'].keys())}")
         #
@@ -191,15 +256,21 @@ if __name__ == "__main__":
                 print(f"components_ {pca.components_.shape}")
         # apply_tsne(data=skeleton_parts_pos_array[:, 0:2, :])
         # print(f"body_parts_pos_array[:, 1, :] {body_parts_pos_array[:, 1, :].shape}")
-        data_to_cluster = np.zeros((n_frames, (n_bodyparts*2 + n_skeleton_parts*2)))
+        data_to_cluster = np.zeros((n_frames, (n_bodyparts * 2 + n_skeleton_parts * 2)))
         data_to_cluster[:, :n_bodyparts] = body_parts_pos_array[:, 0, :].transpose()
-        data_to_cluster[:, n_bodyparts:n_bodyparts*2] = body_parts_pos_array[:, 1, :].transpose()
-        data_to_cluster[:, n_bodyparts*2:(n_bodyparts*2 + n_skeleton_parts)] = \
+        data_to_cluster[:, n_bodyparts:n_bodyparts * 2] = body_parts_pos_array[:, 1, :].transpose()
+        data_to_cluster[:, n_bodyparts * 2:(n_bodyparts * 2 + n_skeleton_parts)] = \
             skeleton_parts_pos_array[:, 0, :].transpose()
-        data_to_cluster[:, (n_bodyparts*2 + n_skeleton_parts):(n_bodyparts*2 + n_skeleton_parts*2)] = \
+        data_to_cluster[:, (n_bodyparts * 2 + n_skeleton_parts):(n_bodyparts * 2 + n_skeleton_parts * 2)] = \
             skeleton_parts_pos_array[:, 1, :].transpose()
         print(f"data_to_cluster.shape {data_to_cluster.shape}")
-        apply_tsne(data=data_to_cluster)
+        behavior_by_frame = None
+        use_cicada_file = True
+        if use_cicada_file:
+            cicada_file = os.path.join(data_path, "p5.npz")
+            behavior_by_frame = encode_frames_from_cicada(cicada_file=cicada_file, n_frames=n_frames,
+                                                        side_to_exclude="right", no_behavior_str="still")
+        apply_tsne(data=data_to_cluster, behavior_by_frame=behavior_by_frame)
         pos_h5_file.close()
         skeketon_h5_file.close()
         raise Exception("H5 OVER")
@@ -213,4 +284,8 @@ if __name__ == "__main__":
     # Preview the first 5 lines of the loaded data
     print(f"pos_df.head() {pos_df.head()}")
     print(f"skeleton_df.head() {skeleton_df.head()}")
+
+if __name__ == "__main__":
+    dlc_behavior_analysis()
+
 
