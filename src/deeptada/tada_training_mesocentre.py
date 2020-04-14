c79c66b69a684645e164fcee4739cf84db061ea5
Julien Denis <julien.denis3@gmail.com>
2020-04-14 06:04:01 +0200

    Square images for tada

--- src/deeptada/tada_training_mesocentre.py
+++ src/deeptada/tada_training_mesocentre.py
@@ -26,14 +26,16 @@ if __name__ == '__main__':
     """
     # "config_tada.yaml"
     # partly_trained_model = "/media/julien/Not_today/hne_not_today/data/test_cinac_gui/transient_classifier_full_model_02-0.9817.h5"
-    tada_model = TadaModel(results_path=results_path, n_epochs=15, verbose=2, batch_size=8,
+    tada_model = TadaModel(results_path=results_path, n_epochs=10, verbose=2, batch_size=8,
                            n_gpus=4,
-                           width_crop=1600, height_crop=1000,
+                           width_crop=1100, height_crop=1100,
                            final_height=128,
-                           window_len=50, max_n_transformations=0,
-                           lstm_layers_size=[64, 128], bin_lstm_size=64,
+                           square_image=True,
+                           window_len=75, max_n_transformations=0,
+                           lstm_layers_size=[128, 256], bin_lstm_size=128,
                            action_tags_yaml_file=action_tags_yaml_file,
                            overlap_value=0.9,
+                           ratio_action_no_action=(0.85, 0.15),
                            n_cameras=2,
                            learning_rate_reduction_patience=3,
                            split_movies_in_frames=True,
